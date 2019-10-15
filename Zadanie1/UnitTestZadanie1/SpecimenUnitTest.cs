035dbaa8a4d43ae7773424918dbf9df6527548ea
JakubFl <jakubflaszka@gmail.com>
2019-10-15 16:14:46 +0200

    class Specimen with test was created.

---
+++ Zadanie1/UnitTestZadanie1/SpecimenUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie1;
+
+namespace UnitTestZadanie1
+{
+    [TestClass]
+    public class SpecimenUnitTest
+    {
+        [TestMethod]
+        public void CtorTest()
+        {
+            Book book = new Book("Tom", "C#Start", 455);
+            DateTimeOffset date = new DateTimeOffset(new DateTime(2019, 10, 15));
+            Specimen specimen = new Specimen(book, date, 23.5f, 23,"XRA");
+            Assert.AreEqual<Book>(book, specimen.Book);
+            Assert.AreEqual<DateTimeOffset>(date, specimen.DateOfPurchase);
+            Assert.AreEqual<float>(23.5f, specimen.NetPrice);
+            Assert.AreEqual<int>(23, specimen.Tax);
+            Assert.AreEqual<string>("XRA", specimen.Id);
+        }
+
+        [TestMethod]
+        public void SetterTest()
+        {
+            Book book = new Book("Tom", "C#Start", 455);
+            DateTimeOffset date = new DateTimeOffset(new DateTime(2019, 10, 15));
+            Specimen specimen = new Specimen(null, new DateTimeOffset(), 0, 0, null);
+            specimen.Book = book;
+            Assert.AreEqual<Book>(book, specimen.Book);
+            specimen.DateOfPurchase = date;
+            Assert.AreEqual<DateTimeOffset>(date, specimen.DateOfPurchase);
+            specimen.NetPrice = 25.6f;
+            Assert.AreEqual<float>(25.6f, specimen.NetPrice);
+            specimen.Tax = 10;
+            Assert.AreEqual<int>(10, specimen.Tax);
+            specimen.Id = "RXX";
+            Assert.AreEqual<string>("RXX", specimen.Id);
+        }
+    }
+}
---
+++ Zadanie1/Zadanie1/Specimen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    public class Specimen
+    {
+        public Book Book { get; set; }
+        public DateTimeOffset DateOfPurchase { get; set; }
+        public float NetPrice { get; set; }
+        public int Tax { get; set; }
+        public string Id { get; set; }
+
+        public Specimen(Book book, DateTimeOffset dateOfPurchase, float netPrice, int tax, string id)
+        {
+            this.Book = book;
+            this.DateOfPurchase = dateOfPurchase;
+            this.NetPrice = netPrice;
+            this.Tax = tax;
+            this.Id = id;
+        }
+    }
+}
