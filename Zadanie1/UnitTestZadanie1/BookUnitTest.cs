abc9c917a39d7b3e2be4485be00ab231942b51ad
JakubFl <jakubflaszka@gmail.com>
2019-10-15 15:43:53 +0200

    class Book with test was created

---
+++ Zadanie1/UnitTestZadanie1/BookUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie1;
+
+namespace UnitTestZadanie1
+{
+    [TestClass]
+    public class BookUnitTest
+    {
+        [TestMethod]
+        public void CtorTest()
+        {
+            Book book = new Book("Tom", "C#Start", 455);
+            Assert.AreEqual<string>("Tom", book.Author);
+            Assert.AreEqual<string>("C#Start", book.Title);
+            Assert.AreEqual<int>(455, book.KeyNumber);
+        }
+
+        [TestMethod]
+        public void SettersTest()
+        {
+            Book book = new Book("Tom", "C#Start", 455);
+            book.Author = "Jerry";
+            Assert.AreEqual<string>("Jerry", book.Author);
+            book.Title = "Java";
+            Assert.AreEqual<string>("Java", book.Title);
+            book.KeyNumber = 997;
+            Assert.AreEqual<int>(997, book.KeyNumber);
+        }
+    }
+}
