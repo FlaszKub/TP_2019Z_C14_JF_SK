abdc2907b0d240f6ca94531ade3683e36b669560
JakubFl <jakubflaszka@gmail.com>
2019-10-15 15:30:54 +0200

    Starting project and class Client with test was created

---
+++ Zadanie1/Zadanie1/Client.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Zadanie1
+{
+    public class Client
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set; }
+   
+        public Client(string firstName, string lastName, string id)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Id = id;
+        }
+    }
+}
