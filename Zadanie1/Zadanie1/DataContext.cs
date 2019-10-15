70d4dac696c07584a0c2fa98824a735f84a96c14
JakubFl <jakubflaszka@gmail.com>
2019-10-15 16:48:43 +0200

    DataContext and DataRepository

---
+++ Zadanie1/Zadanie1/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Zadanie1
+{
+    class DataContext
+    {
+        public List<Client> clients = new List<Client>();
+        public Dictionary<int, Book> books = new Dictionary<int, Book>();
+        public ObservableCollection<SaleEvent> saleEvents = new ObservableCollection<SaleEvent>();
+        public List<Specimen> specimens = new List<Specimen>();
+    }
+}
