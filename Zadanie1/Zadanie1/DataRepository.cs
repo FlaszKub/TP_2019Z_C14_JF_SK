e461039a5dbb8752366a4e1a6b27317e608f3f2b
216817 Kudaj Seweryn <216817@edu.p.lodz.pl>
2019-10-17 23:59:38 +0200

    IDataFiller interface was created and added in DataRepository constructor

--- Zadanie1/Zadanie1/DataRepository.cs
+++ Zadanie1/Zadanie1/DataRepository.cs
@@ -5,5 +5,11 @@ namespace Zadanie1
     class DataRepository
     {
         private DataContext dataContext;
+        private IDataFiller filler;
+        public DataRepository(DataContext dataContext, IDataFiller filler) {
+            this.dataContext = dataContext;
+            this.filler = filler;
+            filler.Fill(dataContext);
+        }
     }
 }
