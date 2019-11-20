866bbd4d26b8a23067583cdadf20d6cbd2f275ad
JakubFl <jakubflaszka@gmail.com>
2019-11-20 20:34:49 +0100

    in progres

---
+++ Zadanie2/UnitTestZadanie2/CircleSerializer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using Zadanie2;
+
+namespace UnitTestZadanie2
+{
+    class CircleSerializer
+    {
+        ObjectIDGenerator gen = new ObjectIDGenerator();
+        char separator = ',';
+
+        public void Serialize(ICSerializable serializable,  Stream stream)
+        {
+            StreamWriter outputFile = new StreamWriter(stream);
+            outputFile.WriteLine(serializable.Serialize(gen));
+            outputFile.Flush();
+        }
+
+        public ICSerializable Deserialize(Stream stream)
+        {
+            Dictionary<int, object> refObjectsDict = new Dictionary<int, object>();
+            ICSerializable temp = null;
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                string line;
+                line = sr.ReadLine();
+                string[] splitedObj = line.Split('>');
+                GenerateTempObj(splitedObj, refObjectsDict);
+                string[] firstObjDetails = splitedObj[0].Split(separator);
+
+                switch (firstObjDetails[0])
+                {
+                    case "UnitTestZadanie2.ClassA":
+                        ClassA a = (ClassA)refObjectsDict[int.Parse(firstObjDetails[1])];
+                        a.Deserialize(firstObjDetails, refObjectsDict);
+                        ((ClassB)refObjectsDict[int.Parse(firstObjDetails[4])]).Deserialize(splitedObj[1].Split(separator), refObjectsDict);
+                        temp = a;
+                        break;
+                    case "UnitTestZadanie2.ClassB":
+                        ClassB b = (ClassB)refObjectsDict[int.Parse(firstObjDetails[1])];
+                        b.Deserialize(firstObjDetails, refObjectsDict);
+                        ((ClassA)refObjectsDict[int.Parse(firstObjDetails[3])]).Deserialize(splitedObj[1].Split(separator), refObjectsDict);
+                        temp = b;
+                        break;
+                }
+
+            }
+            return temp;
+        }
+
+        private void GenerateTempObj(string[] splitedObj, Dictionary<int, object> refObjectsDict)
+        {
+            foreach (string obj in splitedObj)
+            {
+                string[] details = obj.Split(separator);
+                string dataType = details[0];
+                switch (dataType)
+                {
+                    case "UnitTestZadanie2.ClassA":
+                        ClassA a = new ClassA();
+                        refObjectsDict.Add(int.Parse(details[1]), a);
+                        break;
+                    case "UnitTestZadanie2.ClassB":
+                        ClassB b = new ClassB();
+                        refObjectsDict.Add(int.Parse(details[1]), b);
+                        break;
+                }
+
+            }
+        }
+
+
+    }
+}
--- Zadanie2/Zadanie2/ICSerializable.cs
+++ Zadanie2/Zadanie2/ICSerializable.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization;
 
 namespace Zadanie2
 {
-    interface ICSerializable
+    public interface ICSerializable
     {
         string Serialize(ObjectIDGenerator gen);
         void Deserialize(string[] data, Dictionary<int, object> refObjectsDict);
