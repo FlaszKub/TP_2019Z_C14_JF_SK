255a0ff75730de520531101928b5dd3c4a394f5d
Sereusz <seweryn.kudaj@gmail.com>
2019-11-20 16:25:40 +0100

    Little fix to previous comit

--- Zadanie2/Zadanie2/CustomSerializer.cs
+++ Zadanie2/Zadanie2/CustomSerializer.cs
@@ -10,18 +10,45 @@ namespace Zadanie2
     {
         string Serialized;
         Dictionary<int, object> refObjectsDict;
+        List<string> Deserialized = new List<string>();
         public CustomSerializer(){
             
         }
-        public void Serialize(DataContext dataContext, Stream stream) {
+        public void Serialize(DataContext dataContext, String path) {
             ObjectIDGenerator gen = new ObjectIDGenerator();
             Serialized = serializeToString(dataContext, gen);
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.WriteLine(Serialized);
+                }
+            }
+        }
+
+        public DataContext Deserialize(String path)
+        {
+            DataContext context = new DataContext();
+
+            return context;
         }
 
         private string serializeToString(DataContext dataContext, ObjectIDGenerator gen)
         {
             String result = "";
-
+            foreach (ICSerializable book in dataContext.books) {
+                result += book.Serialize(gen) + "\n";
+            }
+            foreach (ICSerializable bookState in dataContext.bookStates) {
+                result += bookState.Serialize(gen) + "\n";
+            }
+            foreach (ICSerializable client in dataContext.clients)
+            {
+                result += client.Serialize(gen) + "\n";
+            }
+            foreach (ICSerializable _event in dataContext.events) {
+                result += _event.Serialize(gen) + "\n";
+            }
             return result;
         }
     }
