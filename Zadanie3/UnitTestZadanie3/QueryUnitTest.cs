dd3b050c06d71f5ac770490d36584221a1a72edc
Sereusz <seweryn.kudaj@gmail.com>
2019-12-02 01:50:02 +0100

    Initial konfiguration + Database setup + ORM

---
+++ Zadanie3/UnitTestZadanie3/QueryUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie3;
+
+namespace UnitTestZadanie3
+{
+    [TestClass]
+    public class QueryUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<Product> lista = QueriesClass.GetProductsByName("Flat");
+            
+            foreach(Product p in lista)
+            {
+                Console.WriteLine(p.Name);
+            }
+        }
+    }
+}
---
+++ Zadanie3/Zadanie3/QueriesClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public static class QueriesClass
+    {
+        
+        public static List<Product> GetProductsByName(string namePart)
+        {
+            ProductionDataContext dataContext = new ProductionDataContext();
+            IQueryable<Product> lista = from product in dataContext.Products
+                                         where product.Name.Contains(namePart)
+                                         select product;
+
+
+            return lista.ToList<Product>();
+        }
+    }
+}
