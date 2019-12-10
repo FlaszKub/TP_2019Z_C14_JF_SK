f3db43f61c0f0e272fb125b8e7e85f6c9042286e
JakubFl <jakubflaszka@gmail.com>
2019-12-10 13:14:23 +0100

    ProductsWithoutCategory query and lambda + tests

---
+++ Zadanie3/UnitTestZadanie3/ExtensionMethodsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie3;
+
+namespace UnitTestZadanie3
+{
+    [TestClass]
+    public class ExtensionMethodsTests
+    {
+        [TestMethod]
+        public void GetProductsWithoutCategoryTest()
+        {
+            using (ProductionDataContext dataContext = new ProductionDataContext())
+            {
+                List<Product> products = dataContext.GetTable<Product>().ToList();
+                List<Product> answer = products.GetProductsWithoutCategory();
+                Assert.AreEqual(answer[0].ProductSubcategory, null);
+            }
+        }
+
+        [TestMethod]
+        public void GetProductsWithoutCategoryQueryTest()
+        {
+            using (ProductionDataContext dataContext = new ProductionDataContext())
+            {
+                List<Product> products = dataContext.GetTable<Product>().ToList();
+                List<Product> answer = products.GetProductsWithoutCategoryQuery();
+                Assert.AreEqual(answer[0].ProductSubcategory, null);
+            }
+        }
+    }
+}
---
+++ Zadanie3/Zadanie3/ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public static class ExtensionMethods
+    {
+
+        public static List<Product> GetProductsWithoutCategoryQuery(this List<Product> products)
+        {
+            List<Product> answer = (from product in products
+                                    where product.ProductSubcategory == null
+                                    select product).ToList();
+            return answer;
+        }
+
+        public static List<Product> GetProductsWithoutCategory(this List<Product> products)
+        {
+            return products.Where(p => p.ProductSubcategory == null).ToList();
+        }
+
+
+    }
+}
