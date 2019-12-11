c7aa7a5767e9a0ba8748efb1a6505f1616378534
JakubFl <jakubflaszka@gmail.com>
2019-12-11 10:19:30 +0100

    part4 finished

--- Zadanie3/UnitTestZadanie3/ExtensionMethodsTest.cs
+++ Zadanie3/UnitTestZadanie3/ExtensionMethodsTest.cs
@@ -15,7 +15,7 @@ namespace UnitTestZadanie3
             using (ProductionDataContext dataContext = new ProductionDataContext())
             {
                 List<Product> products = dataContext.GetTable<Product>().ToList();
-                List<Product> answer = products.GetProductsWithoutCategory();
+                List<Product> answer = products.GetProductsWithoutCategoryLambda();
                 Assert.AreEqual(answer[0].ProductSubcategory, null);
             }
         }
@@ -28,6 +28,36 @@ namespace UnitTestZadanie3
                 List<Product> products = dataContext.GetTable<Product>().ToList();
                 List<Product> answer = products.GetProductsWithoutCategoryQuery();
                 Assert.AreEqual(answer[0].ProductSubcategory, null);
+            }
+        }
+
+        [TestMethod]
+        public void ListPGetVendorProductListQueryTest()
+        {
+            using (ProductionDataContext dataContext = new ProductionDataContext())
+            {
+                List<Product> products = dataContext.GetTable<Product>().ToList();
+                List<ProductVendor> vendors = dataContext.GetTable<ProductVendor>().ToList();
+
+                string answer = products.GetVendorProductListQuery(vendors);
+                string[] lines = answer.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+                Assert.IsTrue(lines.Contains("Thin-Jam Hex Nut 16-Advanced Bicycles"));
+                Assert.IsTrue(lines.Contains("HL Crankarm-West Junction Cycles"));
+                Assert.IsTrue(lines.Contains("LL Crankarm-Vision Cycles, Inc."));
+                Assert.AreEqual(460, lines.Length);
+            }
+        }
+
+        [TestMethod]
+        public void pageTest()
+        {
+            using (ProductionDataContext dataContext = new ProductionDataContext())
+            {
+                List<Product> products = dataContext.GetTable<Product>().ToList();
+                List<Product> result = products.GetProductsAsPage(2, 25);
+                Assert.AreEqual<int>(25, result.Count);
+                result = products.GetProductsAsPage(3, 50);
+                Assert.AreEqual<int>(50, result.Count);
             }
         }
     }
--- Zadanie3/Zadanie3/ExtensionMethods.cs
+++ Zadanie3/Zadanie3/ExtensionMethods.cs
@@ -17,11 +17,47 @@ namespace Zadanie3
             return answer;
         }
 
-        public static List<Product> GetProductsWithoutCategory(this List<Product> products)
+        public static List<Product> GetProductsWithoutCategoryLambda(this List<Product> products)
         {
             return products.Where(p => p.ProductSubcategory == null).ToList();
         }
 
+        public static List<Product> GetProductsAsPage(this List<Product> products, int pageNumber, int productsNumber)
+        {
+            return products.Skip(productsNumber * (pageNumber - 1)).Take(productsNumber).ToList();
+        }
 
+        public static string GetVendorProductListQuery(this List<Product> products, List<ProductVendor> vendors)
+        {
+            string result = "";
+            var answer = (from product in products
+                          from vendor in vendors
+                          where vendor.ProductID.Equals(product.ProductID)
+                          select product.Name + "-" + vendor.Vendor.Name).ToList();
+            int indexLastElement = answer.Count - 1;
+            for (int i = 0; i < answer.Count; i++)
+            {
+                result += answer[i];
+                if (i != indexLastElement) result += Environment.NewLine;
+            }
+            return result;
+
+        }
+
+        public static string GetVendorProductListLambda(this List<Product> products, List<ProductVendor> vendors)
+        {
+            string result = "";
+            var answer = products.Join(vendors, product => product.ProductID, vendor => vendor.ProductID, (product, vendor) => product.Name + "-" + vendor.Vendor.Name).ToList();
+            int indexLastElement = answer.Count - 1;
+            for (int i = 0; i < answer.Count; i++)
+            {
+                result += answer[i];
+                if (i != indexLastElement) result += Environment.NewLine;
+            }
+            return result;
+
+        }
     }
+
 }
+
