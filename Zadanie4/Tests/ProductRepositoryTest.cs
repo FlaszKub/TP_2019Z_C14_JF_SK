98afa7905e2bd21040b56f2bb1e151e710110a74
Sereusz <seweryn.kudaj@gmail.com>
2020-01-15 22:09:06 +0100

    DataContext and ProductRepository tests

---
+++ Zadanie4/Tests/ProductRepositoryTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service;
+
+namespace Tests
+{
+    [TestClass]
+    public class ProductRepositoryTest
+    {
+        [TestMethod]
+        public void GetColors()
+        {
+            ProductRepository rep = new ProductRepository();
+            List<string> colors = rep.GetColors();
+            Assert.AreEqual(10, colors.Count);
+
+        }
+
+        [TestMethod]
+        public void GetProductLinesTest()
+        {
+            ProductRepository rep = new ProductRepository();
+            List<string> lines = rep.GetProductLines();
+            Assert.AreEqual(5, lines.Count);
+        }
+
+        [TestMethod]
+        public void GetSizesTest()
+        {
+            ProductRepository rep = new ProductRepository();
+            List<string> sizes = rep.GetSizes();
+            Assert.AreEqual(19, sizes.Count);
+        }
+
+        [TestMethod]
+        public void GetClassesTest()
+        {
+            ProductRepository rep = new ProductRepository();
+            List<string> classes = rep.GetClasses();
+            Assert.AreEqual(3, classes.Count);
+        }
+
+        [TestMethod]
+        public void GetStylesTest()
+        {
+            ProductRepository rep = new ProductRepository();
+            List<string> styles = rep.GetStyles();
+            Assert.AreEqual(3, styles.Count);
+        }
+    }
+}
